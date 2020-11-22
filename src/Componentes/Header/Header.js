d76f202f0b1d1d2f389eb24687625fd405162981
Paulo <pauloh16.pdr@gmail.com>
2020-11-21 22:30:30 -0300

    Adicionar estilo ao Header

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import estilos from "./Header.module.css";
+import {ReactComponent as Dogs} from "../../Assets/dogs.svg";
 
 const Header = () => {
     return (
         <header className={estilos.header}>
-            <nav className="container">
-                <Link to="/" >Home</Link>
-                <Link to="/login" >Login / Criar</Link>
+            <nav className={`container ${estilos.nav}`}>
+                <Link to="/" className={estilos.logo} aria-label="Dogs - Home" >
+                    <Dogs />
+                </Link>
+                <Link to="/login" className={estilos.login} >Login / Criar</Link>
             </nav>
         </header>
     );
